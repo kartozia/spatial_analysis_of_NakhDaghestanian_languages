ca6e2bd8f4314e4ca73cd626306439085ea03a40
agricolamz <agricolamz@gmail.com>
2017-12-27 00:18:26 +0300

    correct data

--- code/visualise.R
+++ code/visualise.R
@@ -1,18 +1,18 @@
-library(dplyr)
+library(tidyverse)
 library(lingtypology)
-setwd("/home/agricolamz/_DATA/OneDrive1/_Work/_Students/2017_b4_Kartozia/data")
-df <- read.csv("allvillages_v1.tsv", sep = "\t")
+setwd("/home/agricolamz/for_work/HSE/students/2017_b4_Kartozia/data/")
+df <- read_tsv("allvillages_v2.tsv")
 
 df %>% 
   filter(dialect != "") %>% 
   rowwise() %>% 
   mutate(dialect_group = first(unlist(str_split(dialect, pattern = ","))))->
-  df
+  df_filtered
 
-map.feature(languages = df$Language,
-            latitude = df$Lat,
-            longitude = df$Lon,
-            features = df$dialect_group,
-            label = df$dialect,
-            popup = df$EngNames,
-            legend = TRUE)
+map.feature(languages = df_filtered$Language,
+            latitude = df_filtered$Lat,
+            longitude = df_filtered$Lon,
+            features = df_filtered$dialect_group,
+            label = paste(df_filtered$Language, df_filtered$dialect),
+            popup = df_filtered$EngNames,
+            legend = FALSE)
